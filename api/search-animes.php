36144043aa2669f0a40c2555a2f0113f84ad9015
Patrick Ly <patrick.ly@etu.unistra.fr>
2024-03-29 00:37:43 +0100

    modification de l'ordre d'affichage des résultats dans la recherche

--- api/search-animes.php
+++ api/search-animes.php
@@ -7,7 +7,7 @@ $animeName = isset($_GET['name']) ? $_GET['name'] : null;
 $database = new Database();
 $pdo = $database->getPdo();
 
-$anime = $pdo->query('SELECT * from anime WHERE id IN (SELECT distinct id FROM anime_all_title WHERE titre LIKE "%' . $animeName . '%")')->fetchAll();
+$anime = $pdo->query('SELECT * from anime WHERE id IN (SELECT distinct id FROM anime_all_title WHERE titre LIKE "%' . $animeName . '%") ORDER BY CASE WHEN titre LIKE "' . $animeName . '%" THEN 1 ELSE 2 END, titre ASC')->fetchAll();
 
 header('Content-Type: application/json');
 echo json_encode($anime);
--- api/search-mangas.php
+++ api/search-mangas.php
@@ -7,7 +7,7 @@ $mangaName = isset($_GET['name']) ? $_GET['name'] : null;
 $database = new Database();
 $pdo = $database->getPdo();
 
-$manga = $pdo->query('SELECT * from manga WHERE id IN (SELECT distinct id FROM manga_all_title WHERE titre LIKE "%' . $mangaName . '%")')->fetchAll();
+$manga = $pdo->query('SELECT * from manga WHERE id IN (SELECT distinct id FROM manga_all_title WHERE titre LIKE "%' . $mangaName . '%") ORDER BY CASE WHEN titre LIKE "' . $mangaName . '%" THEN 1 ELSE 2 END, titre ASC')->fetchAll();
 
 header('Content-Type: application/json');
 echo json_encode($manga);
